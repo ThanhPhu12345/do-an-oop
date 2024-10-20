39e95d170b9f98a645e792d5904d199f7db617a8
nhan-ng05 <thanhnhannguyennn05@gmail.com>
2024-10-20 20:25:26 +0700

    update

--- src/Nguoi/Purchasable.java
+++ src/Nguoi/Purchasable.java
@@ -1,4 +1,3 @@
-
 package Nguoi;
 
 public interface Purchasable {
---
+++ src/quanliSanPham/Product.java
@@ -0,0 +1,13 @@
+package quanliSanPham;
+
+public interface Product {
+  Integer getPrice();
+
+  String getProductModel();
+
+  String getProductCode();
+
+  String getGuaranteePeriod();
+
+  void displaySpecs();
+}
